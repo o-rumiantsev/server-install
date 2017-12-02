a551be793ac44071f0ea56b9b2984702b9913d57
o-rumiantsev <alex.rum12.12.12@gmail.com>
2017-12-03 00:05:15 +0200

    improved installation script

--- install/if.sh
+++ install/if.sh
@@ -3,3 +3,5 @@
 DIR="/home/.tcp-server"
 
 sudo git clone https://github.com/o-rumiantsev/tcp-server-install "$DIR"
+
+# https://raw.githubusercontent.com/o-rumiantsev/tcp-server-install/master/install/if.sh
