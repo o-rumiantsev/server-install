c3b40af3353fb2f507df471e6566f09eefeccb73
o-rumiantsev <alex.rum12.12.12@gmail.com>
2017-12-03 13:23:12 +0200

    fixed logging

--- install/res/log.js
+++ install/res/log.js
@@ -3,10 +3,11 @@
 const fs = require('fs');
 
 const time = new Date().toString();
+const logFile = '/home/.net-server/usr/bin/log/logs.txt';
 
 global.log = (data) => {
   if (data) {
-    fs.writeFile('./log/logs.txt', `${time}: ${data}\n`, { flag: 'a' }, (err) => {
+    fs.writeFile(logFile, `${time}: ${data}\n`, { flag: 'a' }, (err) => {
       if (err) console.error(err.message);
     });
   }
